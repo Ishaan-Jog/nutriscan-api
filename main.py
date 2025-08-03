42e31a6190a9fd0327aa2b5e5563d7d9ac9d7aba
Ishaan Jog <93370167+Ishaan-Jog@users.noreply.github.com>
2025-08-03 21:49:00 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+# main.py
+from fastapi import FastAPI
+import requests
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "NutriScan API is live!"}
+
+@app.get("/scan/{barcode}")
+def get_product_info(barcode: str):
+    url = f"https://world.openfoodfacts.org/api/v0/product/{barcode}.json"
+    res = requests.get(url)
+    data = res.json()
+
+    if data.get("status") == 1:
+        product = data["product"]
+        return {
+            "name": product.get("product_name", "N/A"),
+            "brand": product.get("brands", "N/A"),
+            "quantity": product.get("quantity", "N/A"),
+            "ingredients": product.get("ingredients_text", "N/A"),
+            "nutriments": product.get("nutriments", {})
+        }
+    else:
+        return {"error": "Product not found"}
